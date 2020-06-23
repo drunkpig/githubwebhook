47fcc960d04a938a97ed1464492c551eac3fb41d
jon <xucaoh@gmail.com>
2020-06-23 18:09:52 +0800

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="github-webhook2",
-    version="0.0.2",
+    version="0.0.3",
     author="drunkpig",
     author_email="xuchaoo@gmail.com",
     description="github webhook",
